63ee95abf22fa350b78edd35c093449a8a8833b5
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2022-02-11 01:22:53 -0300

    feat: funcionalidade de apagar livro finalizada.

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { IndexController } from '../controllers/Book'
+import { IndexController, DestroyController } from '../controllers/Book'
 
 const booksRoutes = Router()
 
 const indexController = new IndexController()
+const destroyController = new DestroyController()
 
 booksRoutes.get('/books', indexController.index)
+booksRoutes.delete('/books/:id', destroyController.destroy)
 
 export { booksRoutes }
