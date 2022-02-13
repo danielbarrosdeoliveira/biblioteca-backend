11193415e1f98e8d89d66c285d38d3f2c38d15ba
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2022-02-13 16:01:20 -0300

    build: test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,9 @@
+import { Request, Response, Router } from 'express'
+
 export * from './books.routes'
+
+const router = Router()
+
+router.get('/', (request: Request, response: Response) => {
+  response.json({ hello: 'world!' })
+})
