b298f2da22a15f45e0020365b1f94fc933a0f9ca
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2022-02-19 18:40:11 -0300

    chore: adjusment index Service

--- src/services/Book/IndexService.ts
+++ src/services/Book/IndexService.ts
@@ -2,7 +2,11 @@ import prismaClient from '../../prisma'
 
 class IndexService {
   async execute() {
-    const books = await prismaClient.book.findMany()
+    const books = await prismaClient.book.findMany({
+      orderBy: {
+        created_at: 'asc'
+      }
+    })
 
     return books
   }
