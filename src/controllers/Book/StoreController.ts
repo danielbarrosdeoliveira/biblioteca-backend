8dcd741ef32283a7dfdcf11be6cb3fa1b9dbc3fd
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2022-02-20 02:37:03 -0300

    refactor: ajuste no controller de criação

--- src/controllers/Book/StoreController.ts
+++ src/controllers/Book/StoreController.ts
@@ -3,13 +3,13 @@ import { StoreService } from '../../services/Book'
 
 class StoreController {
   async store(request: Request, response: Response) {
-    const data = request.body
+    const book = request.body
 
     const storeService = new StoreService()
 
     try {
-      const book = await storeService.execute(data)
-      return response.status(200).json({ book })
+      const result = await storeService.execute(book)
+      return response.status(200).json({ result })
     } catch (error) {
       return response.status(400).json({ error: error.message })
     }
